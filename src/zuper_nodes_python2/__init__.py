17dc797f5f94cb819d4fbf195d27fbe0e86dda33
Andrea Censi <acensi@ethz.ch>
2019-10-13 22:27:01 +0200

    fix

--- src/zuper_nodes_python2/__init__.py
+++ src/zuper_nodes_python2/__init__.py
@@ -6,7 +6,7 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-logging.info('zn-p2 %s' % __version__)
+logger.info('zn-p2 %s' % __version__)
 
 from .imp import *
 from .outside import *
